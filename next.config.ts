c6b3b5096860138557f0d7079f90d51fa44c82ef
rahul gupta <rahul.v.gupta@relianceada.com>
2025-05-05 01:27:13 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // this will create build at /out directory
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Optional: Disable ESLint during build
+  },
+  trailingSlash: true,
+  basePath: '/next-stuff',
+  assetPrefix: '/next-stuff',
 };
 
 export default nextConfig;
